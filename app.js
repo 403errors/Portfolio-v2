0742f565b7cdd5dbe6e0687702700c20f67427d2
Saitama_IITM <95044569+Sitama-from-IITM@users.noreply.github.com>
2023-07-14 18:31:01 +0530

    partials (ejs) + ml project section

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const ejs = require("ejs");
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('home');
 });
 
 app.listen(3000, function(req, res) {
